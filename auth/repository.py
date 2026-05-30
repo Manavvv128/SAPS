b7cb6cfebe60de8f1dac8bfb646863c95c9ebc29
Manavvv128 <manavtaparia128@gmail.com>
2026-05-30 12:10:35 +0530

    Restructure project into backend and frontend directories

--- auth/repository.py
+++
@@ -1,9 +0,0 @@
-# auth/repository.py
-from typing import Optional, Dict, Any
-import database
-
-def create_user(user: Dict[str, Any]) -> None:
-    database.users[user["email"]] = user
-
-def get_user_by_email(email: str) -> Optional[Dict[str, Any]]:
-    return database.users.get(email, None)
